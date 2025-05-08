abe6dddcd44d8a36cf6e246984d981c99916240f
pkzinga <bsc-com-ne-05-22@gmail.com>
2025-05-08 16:23:51 +0200

    starting main assignment

---
+++ webbrowser.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import QApplication,QWidget
+from PyQt5.QtGui import QPixmap,QIcon
+from PyQt5.QtCore import Qt,QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from pathlib import Path
+
+
+
+
+def main():
+    app = QApplication(sys.argv)
+    sys.exit(app.exec_())
+
+if __name__ == "__main_-":
+    main()
